942e17e96c071f693077ffc14f87c66e9adb6ff4
Doug Parker <dgp1130@users.noreply.github.com>
2024-10-11 21:48:41 -0700

    Only propagate body if present in response.
    With browser cache enabled, somehow the responses had no body and were failing.

--- apps/proxy/src/app/proxy.service.ts
+++ apps/proxy/src/app/proxy.service.ts
@@ -3,35 +3,37 @@ import { Request, Response } from 'express';
 
 @Injectable()
 export class ProxyService {
-    async proxy(req: Request, res: Response, host: string): Promise<Response> {
-        const backendUrl = new URL(host);
-        backendUrl.pathname = req.path;
+  async proxy(req: Request, res: Response, host: string): Promise<Response> {
+    const backendUrl = new URL(host);
+    backendUrl.pathname = req.path;
 
-        // Duplicate the incoming request but targeted at the given backend.
-        const backendRes = await fetch(backendUrl, {
-          method: req.method,
-          headers: req.headers as Record<string, string>,
-          body: req.method !== 'GET' && req.method !== 'HEAD'
-            ? req.body
-            : undefined,
-        });
+    // Duplicate the incoming request but targeted at the given backend.
+    const backendRes = await fetch(backendUrl, {
+      method: req.method,
+      headers: req.headers as Record<string, string>,
+      body: req.method !== 'GET' && req.method !== 'HEAD'
+        ? req.body
+        : undefined,
+    });
 
-        // Propagate status and headers.
-        res.status(backendRes.status);
-        backendRes.headers.forEach((value, key) => {
-            res.header(key, value);
-        });
+    // Propagate status and headers.
+    res.status(backendRes.status);
+    backendRes.headers.forEach((value, key) => {
+      res.header(key, value);
+    });
 
-        // Stream the body.
-        const reader = backendRes.body.getReader();
-        while (true) {
-            const { value, done } = await reader.read();
-            if (done) break;
+    // Stream the body.
+    const reader = backendRes.body?.getReader();
+    if (reader) {
+      while (true) {
+        const { value, done } = await reader.read();
+        if (done) break;
 
-            res.write(value);
-        }
-        res.end();
-
-        return res;
+        res.write(value);
+      }
     }
+    res.end();
+
+    return res;
+  }
 }
