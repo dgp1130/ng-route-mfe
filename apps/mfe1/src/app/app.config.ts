f155b77483928196ee642ea7d06340e97a6e881e
Doug Parker <dgp1130@users.noreply.github.com>
2024-10-11 21:49:17 -0700

    Hard navigate on router match failure.
    This is a bit of a hack as it requires the router to fail to navigate, catch the error, let it fail and log to the console, and then perform a hard navigation. This isn't ideal, as there is no way to synchronously catch the error and suppress it without redirecting to another soft navigation, which we can't do here.

--- apps/mfe1/src/app/app.config.ts
+++ apps/mfe1/src/app/app.config.ts
@@ -1,14 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withNavigationErrorHandler } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { APP_BASE_HREF } from '@angular/common';
+
+const NO_MATCH = 4002; // From https://github.com/angular/angular/blob/f84e8ddcd90c81bf9c4232cf8436ebb509856f42/packages/router/src/errors.ts#L15
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes, withNavigationErrorHandler((err) => {
+      if (err.error.code !== NO_MATCH) return; // Ignore
+
+      // Perform a hard navigation.
+      window.location.href = err.url;
+    })),
     { provide: APP_BASE_HREF, useValue: '/' },
   ],
 };
