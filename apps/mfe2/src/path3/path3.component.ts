1e906f374311a0b4a6be9a6f4f49cbbd4421fa50
Doug Parker <dgp1130@users.noreply.github.com>
2024-10-11 22:54:20 -0700

    Moves `NavComponent` into common library.
    This demonstrates how common components can be shared across MFEs.

--- apps/mfe2/src/path3/path3.component.ts
+++ apps/mfe2/src/path3/path3.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavComponent } from "../nav/nav.component";
+import { NavComponent } from '@ng-route-mfe/common';
 
 @Component({
   selector: 'app-path3',
